4da3083fc36f449fc96a362252e7f9dd05401be0
Nicolas Kruchten <nicolas@datacratic.com>
2017-07-14 23:23:29 -0400

    object constancy

--- app.py
+++ app.py
@@ -3,26 +3,23 @@ import dash, dash_core_components as dcc, dash_html_components as html
 import plotly.graph_objs as go
 import math, pandas as pd, numpy as np
 
+
+n = 1000
+random_scores = np.random.normal(0,1,2*n)
+posneg = np.repeat([1,-1], n)
 def generate_roc_curve_df(goodness, skew):
-    n = 10000
-    df = pd.DataFrame(np.array([
-        np.arange(n + n),
-        np.concatenate((np.ones((n,)), np.zeros((n,)))),
-        np.concatenate((np.zeros((n,)), np.ones((n,)))),
-        np.concatenate((
-                        np.random.normal(0+goodness,goodness/2.0+goodness*skew+1.0,n),
-                        np.random.normal(0-goodness,goodness/2.0-goodness*skew+1.0,n)
-                    ))
-    ]).T, columns=["index","pos", "neg", "score"] )
-    df = df.sort_values(by='score', ascending=False)
-    df["tpr"] = (df["pos"].cumsum())/n
-    df["fpr"] = (df["neg"].cumsum())/n
-    df["tnr"] = (n-df["pos"].cumsum())/n
-    df["fnr"] = (n-df["neg"].cumsum())/n
-    df["tp"] = (df["pos"].cumsum())
-    df["fp"] = (df["neg"].cumsum())
-    df["tn"] = n-(df["pos"].cumsum())
-    df["fn"] = n-(df["neg"].cumsum())
+    df = pd.DataFrame(dict(
+        pos = (1+posneg)/2, neg = (1-posneg)/2,
+        score = random_scores*(2+posneg*skew)+posneg*goodness
+    )).sort_values(by='score', ascending=False)
+    df["tp"] = df["pos"].cumsum()
+    df["fp"] = df["neg"].cumsum()
+    df["tn"] = n-df["pos"].cumsum()
+    df["fn"] = n-df["neg"].cumsum()
+    df["tpr"] = df["tp"]/n
+    df["fpr"] = df["fp"]/n
+    df["tnr"] = df["tn"]/n
+    df["fnr"] = df["fn"]/n
     return df
 
 app = dash.Dash()
@@ -34,87 +31,67 @@ app.layout = html.Div([
         html.Label('Skew'),
         dcc.Slider(id='skew', min=-0.4, max=0.4, value=0.0, step=0.1),
         html.Label('Threshold'),
-        dcc.Slider(id='threshold', min=0, max=20000, value=10000, step=100),
+        dcc.Slider(id='threshold', min=0, max=2*n, value=n, step=n/10),
         ], style={'columnCount': 3}),
-    html.Div([
-        dcc.Graph(id='classes'),
-        dcc.Graph(id='rates'),
-        ], style={'columnCount': 2}),
-    html.Div([
-        dcc.Graph(id='roc'),
-        dcc.Graph(id='confusion'),
-        ], style={'columnCount': 2})
+    html.Div(id="charts")
 ])
 
 @app.callback(
-    dash.dependencies.Output('classes', 'figure'),
-    [dash.dependencies.Input('goodness', 'value'),
-     dash.dependencies.Input('skew', 'value')])
-def update_classes(goodness, skew):
-    df = generate_roc_curve_df(goodness, skew)
-    kwargs = dict( opacity=0.75, autobinx=False,
-        xbins=dict( start=-10.0, end=10, size=0.1 )
-        )
-    return dict(
-        data = [
-            go.Histogram(x=df.query('pos==1').score, **kwargs),
-            go.Histogram(x=df.query('neg==1').score, **kwargs)
-        ],
-        layout = go.Layout(
-            barmode='overlay',
-            xaxis=dict( range=[-10, 10] ),
-            yaxis=dict( range=[0, 500] )
-        )
-    )
-
-
-@app.callback(
-    dash.dependencies.Output('rates', 'figure'),
-    [dash.dependencies.Input('goodness', 'value'),
-     dash.dependencies.Input('skew', 'value')])
-def update_rates(goodness, skew):
-    df = generate_roc_curve_df(goodness, skew)
-    return dict(
-        data = [
-            go.Scatter(x=df.score, y=df.tpr),
-            go.Scatter(x=df.score, y=df.fpr),
-        ],
-        layout = go.Layout(
-            xaxis=dict( range=[-10, 10] ),
-            yaxis=dict( range=[0, 1] )
-        )
-    )
-
-
-@app.callback(
-    dash.dependencies.Output('roc', 'figure'),
-    [dash.dependencies.Input('goodness', 'value'),
-     dash.dependencies.Input('skew', 'value')])
-def update_roc(goodness, skew):
-    df = generate_roc_curve_df(goodness, skew)
-    return dict(
-        data = [
-            go.Scatter(x=df.fpr, y=df.tpr)
-        ],
-        layout = go.Layout(
-            xaxis=dict( range=[0, 1] ),
-            yaxis=dict( range=[0, 1] )
-        )
-    )
-
-@app.callback(
-    dash.dependencies.Output('confusion', 'figure'),
+    dash.dependencies.Output('charts', 'children'),
     [dash.dependencies.Input('goodness', 'value'),
      dash.dependencies.Input('skew', 'value'),
      dash.dependencies.Input('threshold', 'value')])
-def update_confusion(goodness, skew, threshold):
+def update_charts(goodness, skew, threshold):
     df = generate_roc_curve_df(goodness, skew)
-    labels = ['tp', 'fp', 'tn', 'fn']
-    return dict(
-        data = [
-            go.Pie(labels=labels, values=df.iloc[threshold][labels], sort=False)
-        ]
+
+    hist_kwargs = dict(
+        opacity=0.75, autobinx=False,
+        xbins=dict( start=-10.0, end=10, size=0.5 )
     )
+
+    return [
+        html.Div([
+            dcc.Graph(id='classes', figure=dict(
+                data = [
+                    go.Histogram(x=df[df.pos==1].score, **hist_kwargs),
+                    go.Histogram(x=df[df.neg==1].score, **hist_kwargs)
+                ],
+                layout = go.Layout(
+                    barmode='overlay',
+                    xaxis=dict( range=[-10, 10] ),
+                    yaxis=dict( range=[0, 200] )
+                )
+            )),
+            dcc.Graph(id='rates', figure=dict(
+                data = [
+                    go.Scatter(x=df.score, y=df.tpr),
+                    go.Scatter(x=df.score, y=df.fpr),
+                ],
+                layout = go.Layout(
+                    xaxis=dict( range=[-10, 10] ),
+                    yaxis=dict( range=[0, 1] )
+                )
+            )),
+            ], style={'columnCount': 2}),
+        html.Div([
+            dcc.Graph(id='roc', figure=dict(
+                data = [
+                    go.Scatter(x=df.fpr, y=df.tpr)
+                ],
+                layout = go.Layout(
+                    xaxis=dict( range=[0, 1] ),
+                    yaxis=dict( range=[0, 1] )
+                )
+            )),
+            dcc.Graph(id='confusion', figure=dict(
+                data = [
+                    go.Pie(labels=['tp','fp','tn','fn'],
+                        values=df.iloc[threshold][['tp','fp','tn','fn']],
+                        sort=False)
+                ]
+            )),
+            ], style={'columnCount': 2})
+        ]
 
 
 if __name__ == '__main__':
